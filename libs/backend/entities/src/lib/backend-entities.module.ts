246e07f576e2cde13e873d34dac408501b7d0298
Odhiambo Dormnic <ayimdomnic@outlook.com>
2022-10-08 14:39:42 +0300

    chore: load all modules to the entry point

--- libs/backend/entities/src/lib/backend-entities.module.ts
+++ libs/backend/entities/src/lib/backend-entities.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [],
