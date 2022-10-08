382d7ca4590f838f69804236bc0828641017dc42
Odhiambo Dormnic <ayimdomnic@outlook.com>
2022-10-08 10:14:33 +0300

    fix: automatically load entities

--- libs/backend/entities/src/lib/entities/user.entity.ts
+++ libs/backend/entities/src/lib/entities/user.entity.ts
@@ -1,5 +1,38 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseModel } from './base.entity';
 
 @Entity()
-export class User extends BaseModel {}
+export class User extends BaseModel {
+  @Column({
+    name: 'first_name',
+    nullable: true,
+  })
+  firstName: string;
+
+  @Column({
+    name: 'last_name',
+    nullable: true,
+  })
+  lastName: string;
+
+  @Column({
+    name: 'email',
+    nullable: true,
+  })
+  email: string;
+
+  @Column({
+    name: 'password',
+  })
+  password: string;
+
+  @Column({
+    name: 'email_verification_token',
+  })
+  emailVerificationToken: string;
+
+  @Column({
+    name: 'email_verified_at',
+  })
+  emailVerifiedAt: Date;
+}
