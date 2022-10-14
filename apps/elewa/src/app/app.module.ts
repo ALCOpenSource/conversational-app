005325dae1434e5466ccd3c6f774cf11c425b2cf
John Adeniran <jasolajohn@gmail.com>
2022-10-14 11:27:39 +0100

    fix:issues surrounding the app

--- apps/elewa/src/app/app.module.ts
+++ apps/elewa/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
@@ -10,7 +9,6 @@ import { SigninComponent } from './signin/signin.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NxWelcomeComponent,
     RegisterComponent,
     SigninComponent,
   ],
--- apps/elewa/tailwindddd.config.js
+++ apps/elewa/tailwindddd.config.js
@@ -4,11 +4,11 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html,scss}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
